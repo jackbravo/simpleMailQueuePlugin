430e138918cbcff1d63e6955bfe507c8507eee17
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-05-08 17:06:41 -0500

    Make base filter abstract classes

--- lib/filter/doctrine/PluginMailQueueFormFilter.class.php
+++ lib/filter/doctrine/PluginMailQueueFormFilter.class.php
@@ -7,7 +7,7 @@
  * @subpackage MailQueue *
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
  */
-class MailQueueFormFilter extends BaseMailQueueFormFilter
+abstract class PluginMailQueueFormFilter extends BaseMailQueueFormFilter
 {
   public function configure()
   {
